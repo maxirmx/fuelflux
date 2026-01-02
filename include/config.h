eaeaf32e087388f567d038f5bef2f5e6c502adf5
Maxim Samsonov <m.samsonov@computer.org>
2026-01-03 00:20:48 +0300

    wip

---
+++ include/config.h
@@ -0,0 +1,10 @@
+// Copyright (C) 2025 Maxim [maxirmx] Samsonov (www.sw.consulting)
+// All rights reserved.
+// This file is a part of fuelflux application
+
+#pragma once
+
+#include <string>
+
+const std::string CONTROLLER_UID = "232390330480218";   // 0xD35B9718F65A
+const std::string BACKEND_API_URL = "http://ttft.uxp.ru";
