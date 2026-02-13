5106dfa12d47e5af88df22521cb8c3ee4411df31
Maxim Samsonov <maxirmx@sw.consulting>
2026-02-13 10:15:42 +0300

    Refactor backend host parsing into shared URL utility

---
+++ src/url_utils.cpp
@@ -0,0 +1,46 @@
+// Copyright (C) 2025, 2026 Maxim [maxirmx] Samsonov (www.sw.consulting)
+// All rights reserved.
+// This file is a part of fuelflux application
+
+#include "url_utils.h"
+
+#include <string_view>
+
+namespace fuelflux {
+
+std::string ExtractHostFromUrl(const std::string& url) {
+    std::string_view urlToParse(url);
+
+    const auto schemePos = urlToParse.find("://");
+    if (schemePos != std::string_view::npos) {
+        urlToParse.remove_prefix(schemePos + 3);
+    }
+
+    const auto atPos = urlToParse.rfind('@');
+    if (atPos != std::string_view::npos) {
+        urlToParse.remove_prefix(atPos + 1);
+    }
+
+    if (!urlToParse.empty() && urlToParse.front() == '[') {
+        const auto bracketEnd = urlToParse.find(']');
+        if (bracketEnd != std::string_view::npos) {
+            return std::string(urlToParse.substr(1, bracketEnd - 1));
+        }
+    }
+
+    const auto portPos = urlToParse.find(':');
+    const auto pathPos = urlToParse.find('/');
+
+    if (portPos != std::string_view::npos &&
+        (pathPos == std::string_view::npos || portPos < pathPos)) {
+        return std::string(urlToParse.substr(0, portPos));
+    }
+
+    if (pathPos != std::string_view::npos) {
+        return std::string(urlToParse.substr(0, pathPos));
+    }
+
+    return std::string(urlToParse);
+}
+
+} // namespace fuelflux
